d0391ac7b716efec86f700636eb6878234880f23
kbuzard <kbuzard@gmail.com>
2015-06-15 12:58:03 -0400

    finish converting machina triangle graph program to R

--- machina_triangle_graph.R
+++ machina_triangle_graph.R
@@ -6,12 +6,14 @@ WB = 10 #willingness to pay for vote buyer B
 al = .5 #alpha
 
 v = seq(0.01,WB,0.01) #this will be counter variable in loop
-out = matrix(0,length(v),5) #pre-reserve space in output matrix
+out = matrix(NA,length(v),5) #pre-reserve space in output matrix
 
 #this loop goes through each value of expenditure up to the willingness-to-pay
 #creating matrix "out" with the probability of winning, expected value, and location
 #in meshgrid space of the optimum (later code will translate into $ values)
-for (t in v) {
+for (j in 1:length(v)) {
+  t = v[j]
+  
   b0 = seq(0,t,.01*t) #possible bribe values for legislator located at 0
   bp5 = seq(0,t,.01*t) #possible bribe values for legislator located at 0.5
 
@@ -32,18 +34,18 @@ for (t in v) {
   ind = which.max(obj2) #the location at which the objective fcn is maximized
   RC = arrayInd(ind,c(dim(obj2),dim(obj2))) #row/column version of maximand location
   
-  out[t,1] = t #I needed a loop counter in Matlab; may not need it in R
-  out[t,2] = (value + B0[RC[1],RC[2]] + Bp5[RC[1],RC[2]] + Bnp5[RC[1],RC[2]])/WB #backs out probability of winning
-  out[t,3] = value #expected value
-  out[t,4] = RC[1] #row location of maximand: gives bribe to 0.5 legislator
-  out[t,5] = RC[2] #column location of maximand: gives bribe to 0.0 legislator
+  out[j,1] = t #I needed a loop counter in Matlab; may not need it in R
+  out[j,2] = (value + B0[RC[1],RC[2]] + Bp5[RC[1],RC[2]] + Bnp5[RC[1],RC[2]])/WB #backs out probability of winning
+  out[j,3] = value #expected value
+  out[j,4] = RC[1] #row location of maximand: gives bribe to 0.5 legislator
+  out[j,5] = RC[2] #column location of maximand: gives bribe to 0.0 legislator
 }
 
 #rest of program in Matlab code: won't make any sense if "out" matrix
 #isn't properly populated through above loop
-[v, l] = max(out(:,3));
-bribe0 = (out(l,5)-1)*l/10000
-bribep5 = (out(l,4)-1)*l/10000
+l = which.max(out[,3])
+bribe0 = (out[l,5]-1)*l/10000
+bribep5 = (out[l,4]-1)*l/10000
 bribenp5 = l/100 - bribe0 -bribep5
 
 z = -.5 - al + bribenp5
