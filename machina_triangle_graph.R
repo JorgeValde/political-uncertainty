4fb5860690d2c66e2a9d30b1b87134023f681cbb
kbuzard <kbuzard@gmail.com>
2015-07-03 09:57:03 -0400

    add comments to sanity check portion of machina_triangle_graph.R

--- machina_triangle_graph.R
+++ machina_triangle_graph.R
@@ -41,17 +41,18 @@ for (j in 1:length(v)) {
   out[j,5] = RC[2] #column location of maximand: gives bribe to 0.0 legislator
 }
 
-#rest of program in Matlab code: won't make any sense if "out" matrix
-#isn't properly populated through above loop
-l = which.max(out[,3])
-bribe0 = (out[l,5]-1)*l/10000
-bribep5 = (out[l,4]-1)*l/10000
-bribenp5 = l/100 - bribe0 -bribep5
+#calculate bribes for maximal value
+l = which.max(out[,3])             #maximal value
+bribe0 = (out[l,5]-1)*l/10000      #scale column location of max value for total spent (l)
+bribep5 = (out[l,4]-1)*l/10000     #likewise scale row column for 0.5 legislator
+bribenp5 = l/100 - bribe0 -bribep5 # -0.5 legislator is calculated as residual
 
-z = -.5 - al + bribenp5
-x = - al + bribe0
+z = -.5 - al + bribenp5            #some theoretical checkpoints in terms of
+x = - al + bribe0                  # x,y and z; also makes calculation below cleaner
 y = .5 - al + bribep5
 
-lhs_y = (exp(-x)+exp(-z))/((1+exp(-x))*(1+exp(-z)))*(exp(-y)/((1+exp(-y))^2))
-lhs_x = (exp(-y)+exp(-z))/((1+exp(-y))*(1+exp(-z)))*(exp(-x)/((1+exp(-x))^2))
-lhs_z = (exp(-y)+exp(-x))/((1+exp(-y))*(1+exp(-x)))*(exp(-z)/((1+exp(-z))^2))
+#Right hand side should equal 1 for legislators with non-zero bribes;
+#Right hand side should be less than 1 for those with zero bribes b/c lambda > 0
+lhs_y = WB*(exp(-x)+exp(-z))/((1+exp(-x))*(1+exp(-z)))*(exp(-y)/((1+exp(-y))^2))
+lhs_x = WB*(exp(-y)+exp(-z))/((1+exp(-y))*(1+exp(-z)))*(exp(-x)/((1+exp(-x))^2))
+lhs_z = WB*(exp(-y)+exp(-x))/((1+exp(-y))*(1+exp(-x)))*(exp(-z)/((1+exp(-z))^2))
