59fe7af5d7915b65382c750ad13daf679f838b54
kbuzard <kbuzard@gmail.com>
2015-08-14 10:02:09 -0400

    update nlm.m for change from nlm to optim for constrainted optimization

--- nlm.r
+++ nlm.r
@@ -1,20 +1,15 @@
-WB = 10
+WB = 10 #willingness to pay parameter
 al = .5 #alpha
 
-Z = -.5 - al + Bnp5 #these are shorthand variables for the exponents
-X = - al + B0       #in the logistic CDFs
-Y = .5 - al + Bp5
+Z = -.5 - al + 0         #these are shorthand variables for the exponents
+X = - al + 1.93007       #in the logistic CDFs; I don't use them in the function
+Y = .5 - al + 1.430073   #but I've pasted in values here to check
 
--X = al - B0
--Y = al - .5 - Bp5
--Z = al + .5 - Bnp5
+# -X = al - B0           These were just reminder lines for when I substituted
+# -Y = al - .5 - Bp5     back to the fundamental expressions for B0, Bp5 and Bnp5
+# -Z = al + .5 - Bnp5    in the objective function "obj" below
 
-fr <- function(x) {   ## Rosenbrock Banana function
-  x1 <- x[1]
-  x2 <- x[2]
-  100 * (x2 - x1 * x1)^2 + (1 - x1)^2
-}
-
+#the objective function with a negative sign since optim in a minimizer
 obj <- function(B) {
   B0 <- B[1]
   Bp5 <- B[2]
@@ -22,8 +17,6 @@ obj <- function(B) {
   -10*((1/(1+exp(.5 - B0)))*(1/(1+exp(.5 - .5 - Bp5)))*(1-1/(1+exp(.5 + .5 - Bnp5))) + (1/(1+exp(.5 - B0)))*(1/(1+exp(.5 + .5 - Bnp5)))*(1-1/(1+exp(.5 - .5 - Bp5))) + (1/(1+exp(.5 + .5 - Bnp5)))*(1/(1+exp(.5 - .5 - Bp5)))*(1-1/(1+exp(.5 - B0))) + (1/(1+exp(.5 - B0)))*(1/(1+exp(.5 - .5 - Bp5)))*(1/(1+exp(.5 + .5 - Bnp5)))) + B0 + Bp5 + Bnp5
 }
 
-obj <- function(B0,Bp5,Bnp5) -10*((1/(1+exp(.5 - B0)))*(1/(1+exp(.5 - .5 - Bp5)))*(1-1/(1+exp(.5 + .5 - Bnp5))) + (1/(1+exp(.5 - B0)))*(1/(1+exp(.5 + .5 - Bnp5)))*(1-1/(1+exp(.5 - .5 - Bp5))) + (1/(1+exp(.5 + .5 - Bnp5)))*(1/(1+exp(.5 - .5 - Bp5)))*(1-1/(1+exp(.5 - B0))) + (1/(1+exp(.5 - B0)))*(1/(1+exp(.5 - .5 - Bp5)))*(1/(1+exp(.5 + .5 - Bnp5)))) + B0 + Bp5 + Bnp5
-
-nlm(obj,c(0,0,0))
-
+#optim function minimizes 'obj'; c vector are starting values; lower is the
+#constraint; I choose a null gradient because it was easiest to get up and running
 optim(c(0.01,0.01,0.01),obj,gr=NULL,method = "L-BFGS-B", lower = c(0,0,0))
