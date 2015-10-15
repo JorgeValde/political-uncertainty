0338e969b758a94f9d0044e9957c067f1a3ff52d
Kristy Buzard <kbuzard@gmail.com>
2015-10-15 13:39:45 -0400

    add FOC to optimization problem for checking purposes

--- constrained-optimization.r
+++ constrained-optimization.r
@@ -1,4 +1,4 @@
-WB = 7 #willingness to pay parameter
+WB = 8.48 #willingness to pay parameter
 al = .5 #alpha
 
 #the objective function with a negative sign since optim in a minimizer
@@ -16,6 +16,8 @@ optim(c(0.01,0.01,0.01),obj,gr=NULL,method = "L-BFGS-B", lower = c(0,0,0))
 X = - al + 1.905965        #these are shorthand variables for the exponents
 Y = .5 - al + 1.405956   #in the logistic CDFs; I don't use them in the function
 Z = -.5 - al + 2.406003         #but I've pasted in values here to check
+
+((exp(-Z)+exp(-Y))*exp(-X))/((1+exp(-Z))*((1+exp(-X))^2)*(1+exp(-Y))) #FOC for X
 
 # -X = al - B0           These were just reminder lines for when I substituted
 # -Y = al - .5 - Bp5     back to the fundamental expressions for B0, Bp5 and Bnp5
