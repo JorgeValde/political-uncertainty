ecb5a7e68f7cf460f4c01efba970290c02ebf604
kbuzard <kbuzard@gmail.com>
2015-10-22 09:48:30 -0400

    anonyize optim function

--- constrained-optimization.r
+++ constrained-optimization.r
@@ -1,24 +1,20 @@
-WB = 60 #willingness to pay parameter
-al = .5 #alpha
+#optim function minimizes 'obj'; c vector are starting values; lower is the
+#constraint; I choose a null gradient because it was easiest to get up and running
 
-#the objective function with a negative sign since optim in a minimizer
-obj <- function(B) {
+#I've now inserted the function to be optimized as an anonymous function
+#for now, function "o" takes the alpha parameter and willingness to pay parameter
+#directly. I don't plan to vary alpha much, so that part is fine, except I need to
+#use is to calculate the X,Y and Z below. So I need to pull them into the function
+#or pull the alpha out.
+
+#bigger issue is to loop over the WB
+o <- optim(c(0.01,0.01,0.01,2,.05),(function(B,WB,al) {
   B0 <- B[1]
   Bp5 <- B[2]
   Bnp5 <- B[3]
   -WB*((1/(1+exp(.5 - B0)))*(1/(1+exp(.5 - .5 - Bp5)))*(1-1/(1+exp(.5 + .5 - Bnp5))) + (1/(1+exp(.5 - B0)))*(1/(1+exp(.5 + .5 - Bnp5)))*(1-1/(1+exp(.5 - .5 - Bp5))) + (1/(1+exp(.5 + .5 - Bnp5)))*(1/(1+exp(.5 - .5 - Bp5)))*(1-1/(1+exp(.5 - B0))) + (1/(1+exp(.5 - B0)))*(1/(1+exp(.5 - .5 - Bp5)))*(1/(1+exp(.5 + .5 - Bnp5)))) + B0 + Bp5 + Bnp5
-}
+}),gr=NULL,method = "L-BFGS-B", lower = c(0,0,0))
 
-#optim function minimizes 'obj'; c vector are starting values; lower is the
-#constraint; I choose a null gradient because it was easiest to get up and running
-opt <- optim(c(0.01,0.01,0.01),obj,gr=NULL,method = "L-BFGS-B", lower = c(0,0,0))
-
-X = - al + 1.905965        #these are shorthand variables for the exponents
-Y = .5 - al + 1.405956   #in the logistic CDFs; I don't use them in the function
-Z = -.5 - al + 2.406003         #but I've pasted in values here to check
-
-((exp(-Z)+exp(-Y))*exp(-X))/((1+exp(-Z))*((1+exp(-X))^2)*(1+exp(-Y))) #FOC for X
-
-# -X = al - B0           These were just reminder lines for when I substituted
-# -Y = al - .5 - Bp5     back to the fundamental expressions for B0, Bp5 and Bnp5
-# -Z = al + .5 - Bnp5    in the objective function "obj" below
+X = - al + o$par[1]       #B0   these are shorthand variables for the exponents
+Y = .5 - al + o$par[2]    #Bp5  in the logistic CDFs; I don't use them in the function
+Z = -.5 - al + o$par[3]   #Bnp5 but I've pasted in values here to check
