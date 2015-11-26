85dbd73e09050ddce43a7c1b37b6577fa79e4d69
kbuzard <kbuzard@gmail.com>
2015-11-26 10:54:34 -0500

    change code to output net positions instead of bribes, reorder and round

--- constrained-optimization.r
+++ constrained-optimization.r
@@ -12,26 +12,27 @@ f <- function(B,WB,al) {
 
 o <- optim(c(0.01,0.01,0.01,WB,al),function(B) f(B,WB,al),gr=NULL,method = "L-BFGS-B", lower = c(0,0,0))
 
-X = - al + o$par[1]       #B0   these are shorthand variables for the exponents
-Y = .5 - al + o$par[2]    #Bp5  in the logistic CDFs; I don't use them in the function
-Z = -.5 - al + o$par[3]   #Bnp5 but I've pasted in values here to check
+X = round(- al + o$par[1], digits = 1)       #B0   these are shorthand variables for the exponents
+Y = round(.5 - al + o$par[2], digits = 1)    #Bp5  in the logistic CDFs; I don't use them in the function
+Z = round(-.5 - al + o$par[3], digits=1)   #Bnp5 but I've pasted in values here to check
 
-pos <- c(X,Y,Z)
+pos <- c(Z,X,Y)
 bribes <- c(o$par,-o$value)
-out <- list("solns" = o$par[1:3], "objMax" = -o$value, "a" = al, "wb" = WB)
+out <- list("solns" = o$par[1:3], "pos" = pos, "objMax" = -o$value, "a" = al, "wb" = WB)
 return(out)
 }
 
 # Create a dataframe of parameter values
 wb_vector <- 1:20
-a_vector <- seq(0.1, 0.9, 0.1)
+a_vector <- seq(0.1, 0.9, 0.2)
 params <- expand.grid("wb" = wb_vector, "a" = a_vector)
 
 # Use "Map" to evaluate the "h" function at each pair of parameter values
 results <- Map(h, al = params$a, WB = params$wb)
 
-# Extract solutions *only* and bind to the parameter values
-solns <- lapply(seq_along(results), function(x) results[[x]]$solns)
+# Extract positions and bind to the parameter values
+# WOULD LIKE TO HAVE BOTH SOLUTIONS AND POSITIONS IN OUTPUT VECTOR BUT DON'T KNOW HOW
+solns <- lapply(seq_along(results), function(x) results[[x]]$pos)
 solns <- do.call("rbind", solns)
 colnames(solns) <- c("person1", "person2", "person3")
 solns <- cbind(params, solns)
