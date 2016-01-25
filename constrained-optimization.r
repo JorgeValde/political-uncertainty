45fb287132a558a54516bfc8d9acfca84022dc68
Kristy Buzard <kbuzard@gmail.com>
2016-01-25 18:30:11 -0500

    change rounding to 2 significant digits

--- constrained-optimization.r
+++ constrained-optimization.r
@@ -11,12 +11,12 @@ f <- function(B,WB,al) {
   -WB*((1/(1+exp(al - B0)))*(1/(1+exp(al - .5 - Bp5)))*(1-1/(1+exp(al + .5 - Bnp5))) + (1/(1+exp(al - B0)))*(1/(1+exp(al + .5 - Bnp5)))*(1-1/(1+exp(al - .5 - Bp5))) + (1/(1+exp(al + .5 - Bnp5)))*(1/(1+exp(al - .5 - Bp5)))*(1-1/(1+exp(al - B0))) + (1/(1+exp(al - B0)))*(1/(1+exp(al - .5 - Bp5)))*(1/(1+exp(al + .5 - Bnp5)))) + B0 + Bp5 + Bnp5
 }   
 
-#o <- optim(c(0.01,0.01,0.01,WB,al),function(B) f(B,WB,al),gr=NULL,method = "L-BFGS-B", lower = c(0,0,0))
-o <- constrOptim(c(0.01,0.01,0.01,WB,al),function(B) f(B,WB,al),gr=NULL,method = "Nelder-Mead", ui = rbind(c(1,0,0),c(0,1,0),c(0,0,1)),ci=c(0,0,0))
+o <- optim(c(1,1,1,WB,al),function(B) f(B,WB,al),gr=NULL,method = "L-BFGS-B", lower = c(0,0,0), control = list(maxit=100000))
+#o <- constrOptim(c(0.01,0.01,0.01,WB,al),function(B) f(B,WB,al),gr=NULL,method = "Nelder-Mead", ui = rbind(c(1,0,0),c(0,1,0),c(0,0,1)),ci=c(0,0,0))
 
-X = round(- al + o$par[2], digits = 1)       #B0   these are shorthand variables for the exponents
-Y = round(.5 - al + o$par[3], digits = 1)    #Bp5  in the logistic CDFs; I don't use them in the function
-Z = round(-.5 - al + o$par[1], digits=1)   #Bnp5 but I've pasted in values here to check
+X = round(- al + o$par[2], digits = 2)       #B0   these are shorthand variables for the exponents
+Y = round(.5 - al + o$par[3], digits = 2)    #Bp5  in the logistic CDFs; I don't use them in the function
+Z = round(-.5 - al + o$par[1], digits=2)   #Bnp5 but I've pasted in values here to check
 
 pos <- c(Z,X,Y)
 bribes <- c(o$par,-o$value)
@@ -25,8 +25,8 @@ return(out)
 }
 
 # Create a dataframe of parameter values
-wb_vector <- 7:16 
-a_vector <- seq(-0.81, -0.65, 0.02)
+wb_vector <- 7:15 
+a_vector <- seq(-0.2, 0.2, 0.1)
 params <- expand.grid("wb" = wb_vector, "a" = a_vector)
 
 # Use "Map" to evaluate the "h" function at each pair of parameter values
