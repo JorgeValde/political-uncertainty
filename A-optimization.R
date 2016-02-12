536f00c74d41fe4878f4ec96daf3df63309fea56
Kristy Buzard <kbuzard@gmail.com>
2016-02-11 19:36:30 -0500

    add A's optimization problem

--- A-optimization.R
+++ A-optimization.R
@@ -20,31 +20,37 @@ X = round(-x0 - al + o$par[2], digits = 2)       #B0   these are shorthand varia
 Y = round(-y0 - al + o$par[3], digits = 2)    #Bnp5  in the logistic CDFs; I don't use them in the function
 Z = round(-z0 - al + o$par[1], digits=2)   #Bp5 but I've pasted in values here to check
 
+A0 = x0 
+Ap5 = z0 - .5
+Anp5 = y0 + .5
+
 pos <- c(Z,X,Y)
 bribes <- c(o$par,-o$value)
+bribesA <- c(A0,Anp5,Ap5)
 win <- c(((1/(1+exp(-X)))*(1/(1+exp(-Y)))*(1-1/(1+exp(-Z))) + (1/(1+exp(-X)))*(1/(1+exp(-Z)))*(1-1/(1+exp(-Y))) + (1/(1+exp(-Z)))*(1/(1+exp(-Y)))*(1-1/(1+exp(-X))) + (1/(1+exp(-X)))*(1/(1+exp(-Y)))*(1/(1+exp(-Z)))))
-out <- list("solns" = o$par[1:3], "pos" = pos, "objMax" = -o$value, "a" = al, "wb" = WB, "yInit" = y0, "zInit" = z0, "xInit" = x0, "winProb" = win)
+ValA = 10*(1-win) - A0 - Ap5 - Anp5
+out <- list("solns" = round(o$par[1:3],digits=3), "pos" = pos, "objMax" = -o$value, "a" = al, "wb" = WB, "yInit" = y0, "zInit" = z0, "xInit" = x0, "winProb" = win,"bribesA"=c(A0,Anp5,Ap5,ValA))
 return(out)
 }
 
 # Create a dataframe of parameter values
-x_vector <- seq(0, 1.0, 0.1) 
-y_vector <- seq(-1.0, 0.0, 0.1)
-z_vector <- seq(.5, 1.0, 0.1)
-#params <- expand.grid("wb" = wb_vector, "y" = y_vector)
+x_vector <- seq(0, 1, 0.1) 
+y_vector <- seq(-.5, 1.5, 0.1)
+z_vector <- seq(.5, 1.5, 0.1)
 params <- expand.grid("x" = x_vector, "y" = y_vector, "z" = z_vector)
 
 # Use "Map" to evaluate the "h" function at each pair of parameter values
-#results <- Map(h, y = params$y, WB = params$wb)
-results <- Map(h, z = params$z, x = params$x)
+results <- Map(h, x = params$x, y = params$y, z = params$z)
 
 # Extract positions and bind to the parameter values
 # WOULD LIKE TO HAVE BOTH SOLUTIONS AND POSITIONS IN OUTPUT VECTOR BUT DON'T KNOW HOW
 solns <- lapply(seq_along(results), function(x) results[[x]]$solns)
+bribesA <- lapply(seq_along(results), function(x) results[[x]]$bribesA)
 netpos <- lapply(seq_along(results), function(x) results[[x]]$pos)
 val <- lapply(seq_along(results), function(x) results[[x]]$objMax)
 winProb <- lapply(seq_along(results), function(x) results[[x]]$winProb)
 solns <- do.call("rbind", solns)
+bribesA <- do.call("rbind", bribesA)
 netpos <- do.call("rbind", netpos)
 val <- do.call("rbind", val)
 winProb <- do.call("rbind", winProb)
@@ -52,4 +58,5 @@ colnames(solns) <- c("foe", "middle", "friend")
 colnames(netpos) <- c("Z", "X", "Y")
 colnames(val) <- c("value")
 colnames(winProb) <- c("winProb")
-solns <- cbind(params, solns,netpos,val,winProb)
+colnames(bribesA) <- c("X_a", "Y_a", "Z_a","ValA")
+solns <- cbind(params, solns,netpos,val,winProb,bribesA)
