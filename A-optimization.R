5cf41e18b31ef89612839b9a4ebe930c210ad6af
Kristy Buzard <kbuzard@gmail.com>
2016-02-12 10:02:48 -0500

    fix order of function inputs in A-optimization

--- A-optimization.R
+++ A-optimization.R
@@ -3,9 +3,9 @@
 #note that here the beta parameter is hard coded in as equal to 1
 WA=8
 
-h <- function(WB,al,y0,z0,x0) {
+h <- function(WB,al,x0,y0,z0) {
   
-f <- function(B,WB,al,y0,z0,x0) {
+f <- function(B,WB,al,x0,y0,z0) {
   Bp5 <- B[1]
   B0 <- B[2]
   Bnp5 <- B[3]
@@ -15,11 +15,10 @@ f <- function(B,WB,al,y0,z0,x0) {
 WB=10
 al = 0
 o <- optim(c(1,1,1,WB,al,x0,y0,z0),function(B) f(B,WB,al,x0,y0,z0),gr=NULL,method = "L-BFGS-B", lower = c(0,0,0), control = list(maxit=100000))
-#o <- constrOptim(c(0.01,0.01,0.01,WB,al),function(B) f(B,WB,al),gr=NULL,method = "Nelder-Mead", ui = rbind(c(1,0,0),c(0,1,0),c(0,0,1)),ci=c(0,0,0))
 
-X = round(-x0 - al + o$par[2], digits = 2)       #B0   these are shorthand variables for the exponents
-Y = round(-y0 - al + o$par[3], digits = 2)    #Bnp5  in the logistic CDFs; I don't use them in the function
-Z = round(-z0 - al + o$par[1], digits=2)   #Bp5 but I've pasted in values here to check
+X = round(-x0 - al + o$par[2], digits = 3)       #B0   these are shorthand variables for the exponents
+Y = round(-y0 - al + o$par[3], digits = 3)    #Bnp5  in the logistic CDFs; I don't use them in the function
+Z = round(-z0 - al + o$par[1], digits=3)   #Bp5 but I've pasted in values here to check
 
 A0 = x0 
 Ap5 = z0 - .5
@@ -28,7 +27,7 @@ Anp5 = y0 + .5
 pos <- c(Z,X,Y)
 bribes <- c(o$par,-o$value)
 bribesA <- c(A0,Anp5,Ap5)
-win <- c(((1/(1+exp(-X)))*(1/(1+exp(-Y)))*(1-1/(1+exp(-Z))) + (1/(1+exp(-X)))*(1/(1+exp(-Z)))*(1-1/(1+exp(-Y))) + (1/(1+exp(-Z)))*(1/(1+exp(-Y)))*(1-1/(1+exp(-X))) + (1/(1+exp(-X)))*(1/(1+exp(-Y)))*(1/(1+exp(-Z)))))
+win <- c(((1/(1+exp(-X)))*(1/(1+exp(-Y))) + (1/(1+exp(-X)))*(1/(1+exp(-Z))) + (1/(1+exp(-Z)))*(1/(1+exp(-Y))) - 2*(1/(1+exp(-X)))*(1/(1+exp(-Y)))*(1/(1+exp(-Z)))))
 ValA = WA*(1-win) - A0 - Ap5 - Anp5
 out <- list("solns" = round(o$par[1:3],digits=3), "pos" = pos, "objMax" = -o$value, "a" = al, "wb" = WB, "yInit" = y0, "zInit" = z0, "xInit" = x0, "winProb" = win,"bribesA"=c(A0,Anp5,Ap5,ValA))
 return(out)
